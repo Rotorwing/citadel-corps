26c8d6e23e2e6ca1c345aecbbbbd4342fbb38978
Jomity <60862595+J0m1ty@users.noreply.github.com>
2022-11-14 23:23:00 +0000

    fixed spelling last time

--- load.js
+++ load.js
@@ -2,9 +2,8 @@
 
 const loader = document.createElement("div");
 
-loader.innerHTML = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Rotorwing/citadel-orps@latest/style.css">
-<script src="https://cdn.jsdelivr.net/gh/Rotorwing/citadel-orps@latest/main.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.js"></script>`;
+loader.innerHTML = `<script src="https://cdn.jsdelivr.net/gh/Rotorwing/citadel-corps@V1.2/web/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.min.js"></script>`;
 
 const body = document.querySelector("body");
 body.appendChild(loader);
