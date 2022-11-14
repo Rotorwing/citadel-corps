b95c628aa1d91b3d9be7e7ab122432d821cc661a
Jomity <60862595+J0m1ty@users.noreply.github.com>
2022-11-14 22:59:41 +0000

    loader script

--- load.js
+++ load.js
@@ -1,1 +1,10 @@
-/** Creates the KA page **/
+/** Injects into KA page **/
+
+const loader = document.createElement("div");
+
+loader.innerHTML = `<link  rel="stylesheet" href="style.css">
+<script src="https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/Rotorwing/citadel-orps@lastest/main.js"></script>`;
+
+const body = document.querySelector("body");
+body.appendChild(loader);
