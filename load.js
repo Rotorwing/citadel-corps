2df65a79996f81c6a64ab1e06b1e47bfee225ec9
Jomity <60862595+J0m1ty@users.noreply.github.com>
2022-11-14 23:13:13 +0000

    fixed spelling of file name

--- load.js
+++ load.js
@@ -2,9 +2,9 @@
 
 const loader = document.createElement("div");
 
-loader.innerHTML = `<link  rel="stylesheet" href="style.css">
-<script src="https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/Rotorwing/citadel-orps@lastest/main.js"></script>`;
+loader.innerHTML = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Rotorwing/citadel-orps@latest/style.css">
+<script src="https://cdn.jsdelivr.net/gh/Rotorwing/citadel-orps@latest/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.js"></script>`;
 
 const body = document.querySelector("body");
 body.appendChild(loader);
